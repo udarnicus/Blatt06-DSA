bfc8cdfd2db14b101805d110c695608072ffec57
Radu <udarnicus@gmail.com>
2020-06-09 18:37:00 +0200

    Initial commit

---
+++ Ex06_LastnamesOfMembers_project/src/main/java/de/unistuttgart/vis/dsass2020/ex06/p4/Sorter.java
@@ -0,0 +1,7 @@
+package de.unistuttgart.vis.dsass2020.ex06.p4;
+
+public class Sorter {
+
+	public static <T extends Comparable<T>> void heapSort(ISimpleList<T> list) {
+	}
+}
---
+++ Ex06_LastnamesOfMembers_project/src/test/java/de/unistuttgart/vis/dsass2020/ex06/p4/SorterTest.java
@@ -0,0 +1,14 @@
+package de.unistuttgart.vis.dsass2020.ex06.p4;
+
+import static org.junit.Assert.*;
+
+
+import org.junit.Test;
+
+import de.unistuttgart.vis.dsass2020.ex06.p4.ISimpleList;
+import de.unistuttgart.vis.dsass2020.ex06.p4.Sorter;
+
+public class SorterTest {
+	
+	
+}
