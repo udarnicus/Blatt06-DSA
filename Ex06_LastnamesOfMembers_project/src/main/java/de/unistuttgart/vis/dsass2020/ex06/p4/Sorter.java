5fc8741ed69ce376587877a1aa329bf3798c022b
Radu <udarnicus@gmail.com>
2020-06-10 11:11:23 +0200

    1. First Part of the HeapSort(Aufbau eines Maxheaps) implemented and tested

--- Ex06_LastnamesOfMembers_project/src/main/java/de/unistuttgart/vis/dsass2020/ex06/p4/Sorter.java
+++ Ex06_LastnamesOfMembers_project/src/main/java/de/unistuttgart/vis/dsass2020/ex06/p4/Sorter.java
@@ -3,5 +3,54 @@ package de.unistuttgart.vis.dsass2020.ex06.p4;
 public class Sorter {
 
 	public static <T extends Comparable<T>> void heapSort(ISimpleList<T> list) {
+
 	}
+
+	/**
+	 * creates HeapProperties for MaxHeap
+	 *
+	 * @param list
+	 * @return
+	 */
+
+	public static ISimpleList createHeapProperties(ISimpleList list){
+		int index = (list.size() -1) / 2;
+		while(index >= 0){
+			percolate(list, index, list.size()-1);
+			index--;
+		}
+		return list;
+	}
+
+	/**
+	 * Percolate element for MaxHeap
+	 *
+	 * @param list
+	 * @param idx
+	 * @param last
+	 */
+	private static void percolate(ISimpleList list, int idx, int last) {
+		int i = idx, j;
+		while ((2*i)+1 <= last) { // f[i] hat linkes Kind
+			j = (2*i)+1; // f[j] ist linkes Kind von f[i]
+			if (j+1 <= last) { // f[i] hat auch rechtes Kind
+				if (list.get(j).compareTo(list.get(j+1)) < 0) { // vergleiche Kinder
+					j++; // f[j] ist jetzt kleiner
+				}
+			}
+			if (list.get(i).compareTo(list.get(j)) < 0) { // vergleiche Elter mit Kind
+				list.swap(i, j);
+				i = j; // versickere weiter
+			} else { // halte an, da Heap-Bedingung erfüllt
+				break;
+			}
+		}
+	}
+
+	private static void swap(Comparable[] f, int i1, int i2){
+		Comparable tmp = f[i1]; // f: Feld
+		f[i1] = f[i2];
+		f[i2] = tmp;
+	}
+
 }
--- Ex06_LastnamesOfMembers_project/src/test/java/de/unistuttgart/vis/dsass2020/ex06/p4/SorterTest.java
+++ Ex06_LastnamesOfMembers_project/src/test/java/de/unistuttgart/vis/dsass2020/ex06/p4/SorterTest.java
@@ -9,6 +9,31 @@ import de.unistuttgart.vis.dsass2020.ex06.p4.ISimpleList;
 import de.unistuttgart.vis.dsass2020.ex06.p4.Sorter;
 
 public class SorterTest {
+
+    @Test
+    public void createHeapProperties(){
+        SimpleList<Integer> list = new SimpleList<>();
+        list.append(2);
+        list.append(4);
+        list.append(1);
+        list.append(5);
+        list.append(6);
+        list.append(7);
+        list.append(8);
+        list.append(3);
+
+        for(int i = 0 ; i < list.size(); i++){
+            System.out.println(list.get(i));
+        }
+
+        ISimpleList list1 = Sorter.createHeapProperties(list);
+
+        for(int i = 0 ; i < list1.size(); i++){
+            System.out.println(list1.get(i));
+        }
+
+
+    }
 	
 	
 }
